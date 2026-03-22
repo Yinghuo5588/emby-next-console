059b3a843cd4fb2cb1e0f32631107bb8523a765e
Yinghuo5588 <yinghuo5588@gmail.com>
2026-03-22 11:39:20 +0800

    fix: add missing MediaIcon.vue

---
+++ frontend/src/components/common/icons/MediaIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 16L8.586 11.414C9.366 10.634 10.634 10.634 11.414 11.414L16 16M14 14L15.586 12.414C16.366 11.634 17.634 11.634 18.414 12.414L20 14M6 20H18C19.105 20 20 19.105 20 18V6C20 4.895 19.105 4 18 4H6C4.895 4 4 4.895 4 6V18C4 19.105 4.895 20 6 20Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<script setup lang="ts">
+</script>
